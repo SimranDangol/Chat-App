8bc73b6f5464b73b84034f4bf0879da9f5ad0fd6
SimranDangol <simrandangol10@gmail.com>
2025-05-16 12:45:41 +0545

    changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,13 @@
-// axios.js
+// eslint-disable-next-line no-unused-vars
+import { store } from "@/redux/app/store";
 import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, 
+ const axiosInstance = axios.create({
+  baseURL: "/api/v1",
   withCredentials: true,
 });
 
-export { axiosInstance };
+
+export {axiosInstance};
+
+
